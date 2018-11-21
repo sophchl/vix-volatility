fc9a8e20753a7fcee1c153c7c294228c8bfa710e
Sophia <so9ch11@web.de>
2018-11-21 08:37:35 +0100

    minor changes (R and LaTeX)

--- R/plot_data.R
+++ R/plot_data.R
@@ -1,8 +1,8 @@
 #' Plots and saves time series data
 #'
-#' @param variable
-#' @param title
-#' @param save_name
+#' @param variable the variance that should be plotted
+#' @param title the title as it is written on top of the graph that is plotted
+#' @param save_name the name under which the output should be saved, including file type (e.g.".png")
 #'
 #' @return a file to the latex written folder, in the format specified in save_name
 #' @export
@@ -18,3 +18,5 @@ plot_data1 <- function(variable, title, save_name) {
     ylab(title)
   ggsave(save_name, plot = last_plot(), path = "written/pictures")
 }
+
+
