697e0db427da988394420a146d892f6f1e2ff5e5
Sophia <so9ch11@web.de>
2018-12-08 13:31:17 +0100

    R: fix plot data
    scaling was no longer accurate, as I modified scale of variables

--- R/plot_data.R
+++ R/plot_data.R
@@ -35,8 +35,8 @@ plot_data1 <- function(variable, title, save_name) {
 plot_data2 <- function(infile, save_name){
  plot <-  ggplot(infile, aes(x = Date)) +
     geom_line(aes(y = VIX.Close,  color = "VIX (Close)"), size  = 1) +  ## vix in orange
-    geom_line(aes(y = SP500/20, color = "SP500"), size = 1) + ## SP500 in blue
-    scale_y_continuous(sec.axis = sec_axis(~.*20, name = "S&P 500", breaks = c(400,800,1200,1600,2000)), breaks = c(20,40,60,80,100)) +
+    geom_line(aes(y = SP500/500, color = "SP500"), size = 1) + ## SP500 in blue
+    scale_y_continuous(sec.axis = sec_axis(~.*500, name = "S&P 500", breaks = c(seq(from = 500, to =5000, by = 500))), breaks = c(seq(from = 1, to = 5, by = 1))) +
     scale_x_date(breaks = "2 years", date_labels = "%Y") +
     scale_color_manual(values = c("#00AFBB", "#E7B800")) +
     labs(y = "VIX (Close)", x = "Year", color = "Legend") +
@@ -59,16 +59,16 @@ plot_data2 <- function(infile, save_name){
 #'
 plot_data3 <- function(infile, save_name){
   plot <- ggplot(infile, aes(x = Date)) +
+    geom_line(aes(y = RealizedVolatility, color = "Realized Volatility"), size = 1) + # RV in green
     geom_line(aes(y = VIX.Close,  color = "VIX (Close)"), size  = 1) +  ## vix in orange
-    geom_line(aes(y = SP500/20, color = "SP500"), size = 1) + ## SP500 in blue
-    geom_line(aes(y = RealizedVolatility*200, color = "Realized Volatility"), size = 1) + # RV in green
-    scale_y_continuous(sec.axis = sec_axis(~.*20, name = "S&P 500", breaks = c(400,800,1200,1600,2000)), breaks = c(20,40,60,80,100)) +
+    geom_line(aes(y = SP500/500, color = "SP500"), size = 1) + ## SP500 in blue
+    scale_y_continuous(sec.axis = sec_axis(~.*500,name = "S&P 500", breaks = c(seq(from = 500, to =5000, by = 500))), breaks = c(seq(from = 1, to = 9, by = 1))) +
     scale_x_date(breaks = "2 years", date_labels = "%Y") +
     scale_color_manual(values = c("darkseagreen3", "#00AFBB", "#E7B800")) +
     labs(y = "VIX (Close)", x = "Year", color = "Legend") +
     theme(legend.position = c(0.8,0.9), panel.grid.major = element_line(colour = "grey92"), panel.grid.minor = element_line(colour = "grey92"),
           text = element_text(size = 15), axis.text = element_text(size = 15))
-  ggsave(save_name, plot = last_plot(), width = 16, path = "written/pictures")
+  ggsave(save_name, plot = last_plot(), width = 16, height = 10, path = "written/pictures")
 }
 
 
@@ -85,9 +85,9 @@ plot_data3 <- function(infile, save_name){
 #'
 plot_data4 <- function(infile, save_name){
  plot <-  ggplot(infile, aes(x = Date)) +
-    geom_line(aes(y = VIX.Close/20,  color = "VIX (Close)"), size  = 1) +  ## vix in orange
-    geom_line(aes(y = RealizedVolatility, color = "Realized Volatility"), size = 1) +
-    scale_y_continuous(sec.axis = sec_axis(~.*20, name = "VIX", breaks = c(20,40,60,80,100)), breaks = c(0.1, 0.2, 0.3, 0.4, 0.5), limits = c(0,0.5)) +
+   geom_line(aes(y = RealizedVolatility, color = "Realized Volatility"), size = 1) + # RV in green
+    geom_line(aes(y = VIX.Close,  color = "VIX (Close)"), size  = 1) +  ## vix in orange
+    scale_y_continuous(breaks = c(seq(from = 1, to = 9, by = 1)), limits = c(0,9)) +
     scale_x_date(breaks = "2 years", date_labels = "%Y") +
     scale_color_manual(values = c("darkseagreen3", "#E7B800")) +
     labs(y = "VIX (Close)", x = "Year", color = "Legend") +
@@ -96,4 +96,5 @@ plot_data4 <- function(infile, save_name){
   ggsave(save_name, plot = last_plot(), width = 16, path = "written/pictures")
 }
 
+sec.axis = sec_axis(~.*1, name = "VIX", breaks = c(seq(from = 1, to = 9, by = 1)))
 
