fd3a0f0a091dc50656d4bc4addbac6bbe48331d3
Sophia <so9ch11@web.de>
2018-12-09 20:51:06 +0100

    R: fixed an error and compiled new
    RealizedVariance was still a part of Vol dataset

--- R/clean_data.R
+++ R/clean_data.R
@@ -7,13 +7,14 @@
 #'
 #' @examples
 #' Clean_Vol(Vol_raw)
-Clean_Vol <- function(infile) {
+Clean_Vola <- function(infile) {
   Vol <- (infile[-(1:2),1:2]) # take out columns and rows I do not need
   colnames(Vol) <- c("Date", "RealizedVariance") # rename columns
   Vol$Date <- as.Date(as.character(Vol$Date), "%Y%m%d") # turn Date into appropriate time-based object
   #Vol$RealizedVariance <- as.numeric(as.character(Vol$RealizedVariance)) * 100 * 100
   Vol$RealizedVolatility <- as.numeric(as.character(Vol$RealizedVariance)) %>% sqrt() # turn RealizedVola (daily) to numeric -> Vola = sd
   Vol$RealizedVolatility <- Vol$RealizedVolatility*100
+  Vol <- Vol[,c(1,3)]
   return(Vol)
 }
 
